b4b68bfa0a4fb9b5a5ed4c6e4b9f0459ed3cfdda
John Tims <missionpilot@gmail.com>
2011-04-06 10:17:09 -0500

    Handle command line parameters correctly.

--- main.c
+++ main.c
@@ -9,106 +9,190 @@
 #include "highgui.h"
 #endif
 
+#include <iostream>
+
 #include <stdio.h>  // printf
+#include <ctype.h>  // isdigit
 #include "models.h"
 
-int main(int argc, char *argv[])
+#define ITERATIONS 5
+
+#define WIN_MODIFIED "Modified"
+#define WIN_ORIGINAL "Original"
+
+using namespace std;
+
+IplImage* process_image(IplImage *f, IplImage *u)
 {
-    int I = 5;
+    /*
+    // Let `u` be the current iteration
+    IplImage *u = NULL,
+             *f = NULL;
 
-    CvCapture* capture = NULL;
-
-
-    // Load noisy pgm `f` with Neumann B.C.s
-    IplImage *f = NULL,
-             *t = NULL,
+    f = cvCreateImage(cvSize(t->width, t->height),IPL_DEPTH_8U, 1);
+    cvConvertImage(t, f, 0);
     
-    // Let `u` be the original pgm
+    // Convert to 8 bit grayscale
+    u = cvCreateImage(cvSize(f->width, f->height),IPL_DEPTH_8U, 1);
+    */
+
+    cvCopy(f, u, NULL);
+
+    non_convex(f, u, ITERATIONS);
+    return u;
+}
+
+bool process_image_file(const string s1, const string s2)
+{
+    IplImage *t = cvLoadImage(s1.c_str(), -1);
+
+    if(!t)
+    {
+        cout << "Failed to load image: " << s1 << endl;
+        return false;
+    }
+
+    IplImage *f = cvCreateImage(cvSize(t->width, t->height),IPL_DEPTH_8U, 1),
+             *u = cvCreateImage(cvSize(t->width, t->height),IPL_DEPTH_8U, 1);
+    cvConvertImage(t, f, 0);
+    //cvReleaseImage(&t);
+
+    bool save = !s2.empty();
+
+    if(!(f || u))
+    {
+        return false;
+    }
+
+    process_image(f, u);
+
+    if(save)
+    {
+        cout << "Saving image: `" << s2 << "`" << endl;
+        cvSaveImage(s2.c_str(), u);
+    }
+    else
+    {
+        cout << "Show image" << endl;
+        cvShowImage(WIN_MODIFIED, u);
+        cvShowImage(WIN_ORIGINAL, f);
+    }
+
+    return true;
+}
+
+bool process_video_file(const string s1, const string s2)
+{
+    CvCapture     *capture = NULL;
+    CvVideoWriter *writer  = NULL;
+
+    bool save = !s2.empty();
+
+    if((s1.empty()) || isdigit(s1[0]))
+    {
+        int cam = atoi(s1.c_str());
+        capture = cvCaptureFromCAM(cam);
+    }
+    else
+    {
+        capture = cvCaptureFromAVI(s1.c_str());
+    }
+
+    if(!capture)
+    {
+        return false;
+    }
+
+    IplImage *t = NULL,
+             *f = NULL,
              *u = NULL;
 
-    
-    // load the passed in pgm or try to open
-    // the lena pgm in this directory
-    /*
-    if(argc > 1)
-    {
-        capture = cvCaptureFromAVI(argv[1]);
-
-        if(!capture)
-        {
-            puts("NOT CAPTURING");
-            t = cvLoadImage(argv[1], -1);
-            f = cvCreateImage(cvSize(t->width, t->height),IPL_DEPTH_8U, 1);
-            cvConvertImage(t, f, 0);
-        }else
-        {
-            puts("CAPTURING");
-        }
-    }
-    */
-
-    /*
-    if(!f || !capture)
-    {
-        puts("Invalid image file.");
-        puts("Syntax: ./program file.(jpg|png|pgm|etc)");
-        return 0;
-    }
-    */
-    capture = cvCaptureFromAVI(argv[1]);
-
-    //printf("Image dimensions: %d by %d with %d Channels\n", f->width, f->height, f->nChannels);
-
-    cvNamedWindow("Modified", CV_WINDOW_AUTOSIZE); 
-    cvMoveWindow("Modified", 100, 100);
-
-    cvNamedWindow("Original", CV_WINDOW_AUTOSIZE); 
-    cvMoveWindow("Original", 200, 200);
-
-    t = cvQueryFrame(capture);
-
-    int isColor = 0;
-    int frameH    = (int) cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
-    int frameW    = (int) cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
-    int fps       = (int) cvGetCaptureProperty(capture, CV_CAP_PROP_FPS);
-    int numFrames = (int) cvGetCaptureProperty(capture,  CV_CAP_PROP_FRAME_COUNT);
-    int index = 0;
-
-    CvVideoWriter *writer = cvCreateVideoWriter("out.avi", CV_FOURCC('P','I','M','1'), fps,cvSize(frameW,frameH),isColor);
-
-    if(t)
-    {
-        printf("Video: %dx%d @ %d; %d frames\n", frameH, frameW, fps, numFrames);
-        f = cvCreateImage(cvSize(t->width, t->height),IPL_DEPTH_8U, 1);
-    }
+    bool first = true;
     while(1)
     {
+        t = cvQueryFrame(capture);
+
         if(!t)
         {
             break;
         }
 
-        if(index % 10 == 0)
+        if(first)
         {
-            cvConvertImage(t, f, 0);
-            u = cvCloneImage(f);
+            f = cvCreateImage(cvSize(t->width, t->height),IPL_DEPTH_8U, 1);
+            u = cvCreateImage(cvSize(t->width, t->height),IPL_DEPTH_8U, 1);
 
-            cvShowImage("Original", f);
-
-            non_convex(f, u, I);
-            cvShowImage("Modified", u);
-            cvWaitKey(20);
-            cvWriteFrame(writer, u);
+            if(save)
+            {
+                int isColor = 0;
+                int frameH    = (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
+                int frameW    = (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
+                int fps       = (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FPS);
+                //int numFrames = (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_COUNT);
+                writer = cvCreateVideoWriter(s2.c_str(), CV_FOURCC('P','I','M','1'), fps,cvSize(frameW,frameH),isColor);
+            }
+            first = !first;
         }
 
-        t = cvQueryFrame(capture);
-        ++index;
-    }
+        cvConvertImage(t, f, 0);
+        process_image(f, u);
 
+        if(save)
+        {
+            cvWriteFrame(writer, u);
+        }
+        else
+        {
+            cvWaitKey(20);
+            cvShowImage(WIN_MODIFIED, u);
+            cvShowImage(WIN_ORIGINAL, f);
+        }
+    }
     cvReleaseVideoWriter(&writer);
     cvReleaseCapture(&capture);
-    cvReleaseImage(&f);
-    cvReleaseImage(&u);
+
+    return true;
+}
+
+void print_options(void)
+{
+    puts("Syntax: ./program file.(jpg|png|pgm|etc)");
+}
+
+int main(int argc, char *argv[])
+{
+    string s1 = (argc > 1) ? argv[1] : "",
+           s2 = (argc > 2) ? argv[2] : "";
+
+    cout << "Argv[1]: " << s1 << endl << "Argv[2]: " << s2 << endl;
+
+    cvNamedWindow(WIN_ORIGINAL, CV_WINDOW_AUTOSIZE); 
+    cvMoveWindow(WIN_ORIGINAL, 0, 0);
+
+    cvNamedWindow(WIN_MODIFIED, CV_WINDOW_AUTOSIZE); 
+    cvMoveWindow(WIN_MODIFIED, 200, 200);
+
+    cout << "Trying to process as image" << endl;
+    if(!process_image_file(s1, s2))
+    {
+        cout << "Trying to process as video" << endl;
+        if(!process_video_file(s1, s2))
+        {
+            print_options();
+        }
+    }
+
+    /*
+     * If s2 is empty, then the results are being
+     * displayed in windows, so keep them open
+     * until a key is pressed
+     */
+    if(s2.empty())
+    {
+        cout << "Waiting...." << endl;
+        cvWaitKey(0);
+    }
+
     return 0;
 }
 
